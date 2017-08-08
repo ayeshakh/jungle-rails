bcb9a2a2bdd9e2d2b57c3f6c61c27a3b6cb5815f
Ayesha <ayesha_04@yahoo.com>
2017-08-08 15:34:10 +0000

    edge cases for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,25 @@ class User < ActiveRecord::Base
 
   validates :name, presence:true
   validates :last_name, presence:true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true,
                        length: { minimum: 5 }
   validates :password_confirmation, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.where("LOWER(TRIM(email)) = ?", email.strip.downcase).first
+      if user && user.authenticate(password)
+        user
+      else
+        nil
+      end
+  end
+
 end
+
+ # authentication is only for login purpose the LOWER and TRIM is used in authentication so it manually downcases
+ #the email and removes white space to authenticate but saves the email as the user typed it for the user might have
+ # done it on purpose.
+
+ # the case sensitive in validation is only for the creation of new user to see if the new User being created has the
+ # same email as any other existing user. it also only down cases it for comparison but does not save it like that.
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,5 +89,67 @@ RSpec.describe User, type: :model do
       expect(user.valid?).to be false
       expect(user.errors[:email]).to include("has already been taken")
     end
+
+    it "email must be unique" do
+      User.create({
+        name: "Bob",
+        last_name: "Hanks",
+        email: "bOb@example.com",
+        password: "abcde",
+        password_confirmation: "abcde"
+        })
+
+       user = User.new({
+        name: "Sally",
+        last_name: "Johnson",
+        email: "bob@example.COM",
+        password: "12345",
+        password_confirmation: "12345"
+        })
+      expect(user.valid?).to be false
+      expect(user.errors[:email]).to include("has already been taken")
+    end
+  end
+
+
+  describe '.authenticate_with_credentials' do
+    it "authentication is true" do
+      @user = User.create({
+        name: "Bob",
+        last_name: "Hanks",
+        email: "bob@example.com",
+        password: "abcde",
+        password_confirmation: "abcde"
+      })
+
+      user_auth = User.authenticate_with_credentials(@user.email, @user.password)
+      expect(user_auth).to eql @user
+    end
+
+    it "login with wrong case" do
+      @user = User.create({
+        name: "Bob",
+        last_name: "Hanks",
+        email: "bOb@example.com",
+        password: "abcde",
+        password_confirmation: "abcde"
+      })
+      user_auth = User.authenticate_with_credentials("BOb@EXAMPLE.CoM", @user.password)
+      expect(user_auth).to eql @user
+
+    end
+
+    it "login allowed with whitespace" do
+      @user = User.create({
+        name: "Bob",
+        last_name: "Hanks",
+        email: " bob@example.com ",
+        password: "abcde",
+        password_confirmation: "abcde"
+      })
+
+      user_auth = User.authenticate_with_credentials("  bob@example.com  ", @user.password)
+      expect(user_auth).to eql @user
+    end
   end
 end
